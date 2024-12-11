dd1fad2dd9686138ade21c2f4252a83cf9e0c4da
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-11 14:51:22 +0530

    Create config.py

---
+++ BADUC/core/config.py
@@ -0,0 +1,42 @@
+import os
+import time
+import logging
+
+from os import getenv
+from dotenv import load_dotenv
+from logging.handlers import RotatingFileHandler
+
+
+# config variables
+if os.path.exists("vars.env"):
+    load_dotenv("vars.env")
+API_ID = int(getenv("API_ID", 0))
+API_HASH = getenv("API_HASH", None)
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+STRING_SESSION = getenv("STRING_SESSION", None)
+MONGO_DB_URL = getenv("MONGO_DB_URL", None)
+OWNER_ID = int(getenv("OWNER_ID", 0))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", 0))
+START_IMAGE_URL = getenv("START_IMAGE_URL", None)
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7009601543").split()))
+PM_GUARD = bool(getenv("PM_GUARD", True))
+PM_GUARD_TEXT = getenv("PM_GUARD_TEXT", "**🥀 ʜᴇʏ, ɪ ᴀᴍ ᴀɴ ᴀᴅᴠᴀɴᴄᴇᴅ & ꜱᴜᴘᴇʀꜰᴀꜱᴛ ʜɪɢʜ Qᴜᴀʟɪᴛʏ ᴜꜱᴇʀʙᴏᴛ ᴀꜱꜱɪꜱᴛᴀɴᴛ ᴡɪᴛʜ ᴀɴ ᴜᴘɢʀᴀᴅᴇᴅ ᴠᴇʀꜱɪᴏɴ ꜱᴇᴄᴜʀɪᴛʏ ꜱʏꜱᴛᴇᴍ.\n\n🌿 ɪ ᴄᴀɴ'ᴛ ʟᴇᴛ ʏᴏᴜ ᴍᴇꜱꜱᴀɢᴇ ᴍʏ ᴏᴡɴᴇʀ'ꜱ ᴅᴍ ᴡɪᴛʜᴏᴜᴛ ᴍʏ ᴏᴡɴᴇʀ'ꜱ ᴘᴇʀᴍɪꜱꜱɪᴏɴ.\n\n❤️ ᴍʏ ᴏᴡɴᴇʀ ɪꜱ ᴏꜰꜰʟɪɴᴇ ɴᴏᴡ, ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ ᴜɴᴛɪʟ ᴍʏ ᴏᴡɴᴇʀ ᴀʟʟᴏᴡꜱ ʏᴏᴜ.\n\n🍂 ᴘʟᴇᴀꜱᴇ ᴅᴏɴ'ᴛ ꜱᴘᴀᴍ ʜᴇʀᴇ, ʙᴇᴄᴀᴜꜱᴇ ꜱᴘᴀᴍᴍɪɴɢ ᴡɪʟʟ ꜰᴏʀᴄᴇ ᴍᴇ ᴛᴏ ʙʟᴏᴄᴋ ʏᴏᴜ ꜰʀᴏᴍ ᴍʏ ᴏᴡɴᴇʀ ɪᴅ 👍🏻**")
+PM_GUARD_LIMIT = int(getenv("PM_GUARD_LIMIT", 5))
+
+
+# Don't Edit This Codes From This Line
+
+LOGGER = logging.getLogger("main")
+runtime = time.time()
+
+FLOODXD = {}
+OLD_MSG = {}
+PM_LIMIT = {}
+PLUGINS = {}
+SUDOERS = []
+
+
+COMMAND_HANDLERS = []
+for x in COMMAND_PREFIXES:
+    COMMAND_HANDLERS.append(x)
+COMMAND_HANDLERS.append('')
