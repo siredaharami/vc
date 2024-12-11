6258674fdbdbbd2a0f76f70a8b8ec3b7667b0f31
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-11 14:55:50 +0530

    Update __init__.py

--- BADUC/__init__.py
+++ BADUC/__init__.py
@@ -1,4 +1,11 @@
+import os
 from platform import python_version
 
 import heroku3
 from pyrogram import __version__ as pyrogram_version
+
+__version__ = {
+    "Pbxbot": "3.0",
+    "pyrogram": pyrogram_version,
+    "python": python_version(),
+}
