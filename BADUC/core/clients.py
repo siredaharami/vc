602f7f171640b3cffc869f5a0f59309045d7a70d
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-11 17:00:47 +0530

    Update clients.py

--- BADUC/core/clients.py
+++ BADUC/core/clients.py
@@ -6,7 +6,7 @@ from pytgcalls import PyTgCalls, filters as pytgfl
 from pytgcalls.types import Call, MediaStream, AudioQuality, VideoQuality
 from motor.motor_asyncio import AsyncIOMotorClient
 
-from .config import Config
+from BADUC.core.config import *
 from .logger import LOGGER
 
 def async_config():
