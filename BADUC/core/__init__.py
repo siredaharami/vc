227d463c7b4b86fd45a45c9d8c4b6babb1efc114
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-11 15:03:54 +0530

    Update __init__.py

--- BADUC/core/__init__.py
+++ BADUC/core/__init__.py
@@ -1,8 +1,12 @@
+from .clients import app, bot, call
 from .config import Config
 from .logger import LOGS
 from .command import command
 
 __all__ = [
+    "app",
+    "bot",
+    "call",
     "Config",
     "LOGS",
     "command",
