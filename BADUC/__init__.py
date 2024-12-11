9b32e214151201b23a6a5b571ddf40de53e16033
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-11 17:33:53 +0530

    Update __init__.py

--- BADUC/__init__.py
+++ BADUC/__init__.py
@@ -1,5 +1,5 @@
-from typing import Union, List, Pattern
 from .core.config import *
+from typing import Union, List, Pattern
 from pyrogram import __version__ as pyrogram_version
 from motor.motor_asyncio import AsyncIOMotorClient as _mongo_async_
 from pytgcalls import PyTgCalls, filters as pytgfl
