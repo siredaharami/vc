c20f8857a2c2e8aec460a3db66f4e6ec2c1c0389
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-11 22:00:15 +0530

    Update __main__.py

--- BADUC/__main__.py
+++ BADUC/__main__.py
@@ -3,6 +3,7 @@ import asyncio, importlib
 from pytgcalls import idle
 
 from .core import LOGGER
+from .plugins import ALL_PLUGINS
 from .core.clients import run_async_clients
 from .modules.clients.enums import run_async_enums
 from .modules.helpers.inline import run_async_inline
