9ab80589cad54e474a1d1d5836c85fdab6f7d9ba
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-11 16:14:01 +0530

    Update __init__.py

--- BADUC/__init__.py
+++ BADUC/__init__.py
@@ -1,9 +1,11 @@
-import os
-from platform import python_version
+from typing import Union, List, Pattern
+from .core import  LOGS, Config
+from motor.motor_asyncio import AsyncIOMotorClient as _mongo_async_
+from pytgcalls import PyTgCalls, filters as pytgfl
+from pytgcalls.types import ChatUpdate, Update, GroupCallConfig
+from pytgcalls.types import Call, MediaStream, AudioQuality, VideoQuality
 
-import heroku3
-from pyrogram import __version__ as pyrogram_version
-
+# Ai-Userbot Version
 __version__ = {
     "Pbxbot": "3.0",
     "pyrogram": pyrogram_version,
@@ -14,3 +16,12 @@ spam_chats = []
 SUDO_USER = SUDO_USERS
 OWNER_USERNAME = OWNER_USERNAME
 SUDO_USERS.append(OWNER_ID)
+
+
+
+from core. import LOGS, Config
+
+mongo_async_cli = _mongo_async_(config.MONGO_DB_URL)
+mongodb = mongo_async_cli.badmundaxdb
+
+
