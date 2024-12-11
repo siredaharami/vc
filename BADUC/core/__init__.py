a910a97ed1423e0e8db38fa915d273f9a05fd5f0
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-11 17:30:39 +0530

    Update __init__.py

--- BADUC/core/__init__.py
+++ BADUC/core/__init__.py
@@ -1,14 +1,15 @@
 from .clients import app, bot, call
 from .config import *
 from .logger import LOGGER
-from .command import commandx, commandz
+from .command import cdx, rgx, cdz
 
 __all__ = [
     "app",
     "bot",
     "call",
     "Config",
-    "LOGS",
-    "commandx",
-    "commandz",
+    "LOGGER",
+    "rgx",
+    "cdz",
+    "cdx",
 ]
