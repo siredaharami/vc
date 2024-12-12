1944b8dfc6eda50eb38bae0508c6c2c64be5b427
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-12 21:53:41 +0530

    Update __init__.py

--- BADUC/core/__init__.py
+++ BADUC/core/__init__.py
@@ -1,13 +1,6 @@
 from .clients import app, bot, call
 from .config import *
-from .logger import LOGGER
-logs = LOGGER
 
-from BADUC.core.config import PLUGINS
-plugs = PLUGINS
-
-from BADUC.core import config
-vars = config
 
 from .command import bad, sukh, abhi
 
