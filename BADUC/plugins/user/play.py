acce6c9f18861ca6fcb70e1e580ef56e658b4942
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-12 11:55:15 +0530

    Update play.py

--- BADUC/plugins/user/play.py
+++ BADUC/plugins/user/play.py
@@ -1,3 +1,48 @@
 import aiohttp, aiofiles, asyncio, base64, logging
 import os, platform, random, re, socket
 import sys, time, textwrap
+
+from BADUC.core.clients import *
+from BADUC.core.config import *
+from BADUC import *
+from BADUC.plugins.user.play import *
+from os import getenv
+from io import BytesIO
+from time import strftime
+from functools import partial
+from dotenv import load_dotenv
+from datetime import datetime
+from typing import Union, List, Pattern
+from logging.handlers import RotatingFileHandler
+
+from typing import Union, List, Pattern
+from pyrogram import Client, filters as pyrofl
+from git.exc import GitCommandError, InvalidGitRepositoryError
+from motor.motor_asyncio import AsyncIOMotorClient as _mongo_async_
+
+from pyrogram import Client, filters as pyrofl
+from pytgcalls import PyTgCalls, filters as pytgfl
+
+
+from pyrogram import idle, __version__ as pyro_version
+from pytgcalls.__version__ import __version__ as pytgcalls_version
+
+from ntgcalls import TelegramServerError
+from pyrogram.enums import ChatMemberStatus, ChatType
+from pyrogram.errors import (
+    ChatAdminRequired,
+    FloodWait,
+    InviteRequestSent,
+    UserAlreadyParticipant,
+    UserNotParticipant,
+)
+from pytgcalls.exceptions import NoActiveGroupCall
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pytgcalls.types import ChatUpdate, Update, GroupCallConfig
+from pytgcalls.types import Call, MediaStream, AudioQuality, VideoQuality
+
+from PIL import Image, ImageDraw, ImageEnhance
+from PIL import ImageFilter, ImageFont, ImageOps
+from youtubesearchpython.__future__ import VideosSearch
+
+call_config = GroupCallConfig(auto_start=False)
