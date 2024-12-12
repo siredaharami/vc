43745991547370dcb5606b7bb323e8c7a11ec7aa
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-12 14:15:37 +0530

    Update command.py

--- BADUC/core/command.py
+++ BADUC/core/command.py
@@ -4,16 +4,16 @@ from pytgcalls import PyTgCalls, filters as pytgfl
 
 
 # Command filters for bot and app
-def cdx(commands: Union[str, List[str]]):
+def bad(commands: Union[str, List[str]]):
     """Filter commands starting with /, !, or ."""
     return pyrofl.command(commands, prefixes=["/", "!", "."])
 
 
-def cdz(commands: Union[str, List[str]]):
+def sukh(commands: Union[str, List[str]]):
     """Filter commands starting with no prefix or /, !, ."""
     return pyrofl.command(commands, prefixes=["", "/", "!", "."])
 
 
-def rgx(pattern: Union[str, Pattern]):
+def jass(pattern: Union[str, Pattern]):
     """Filter messages matching a regex pattern."""
     return pyrofl.regex(pattern)
