76b716eb4f6388682266d02b8f1a30c594ce9a5b
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-12 19:27:44 +0530

    Create help.py

---
+++ BADUC/plugins/user/help.py
@@ -0,0 +1,127 @@
+import re
+
+from pyrogram import *
+from pyrogram.types import *
+
+from BADUC.core.clients import *
+from BADUC.core.command import *
+from BADUC import __version__
+from BADUC.functions.buttons import *
+from BADUC.functions.inline import *
+from BADUC.functions.text import *
+
+
+@app.on_message(bad(["help"]))
+@sudo_user
+async def inline_help_menu(client, message):
+    image = None
+    try:
+        if image:
+            bot_results = await app.get_inline_bot_results(
+                f"@{bot.me.username}", "help_menu_logo"
+            )
+        else:
+            bot_results = await app.get_inline_bot_results(
+                f"@{bot.me.username}", "help_menu_text"
+            )
+        await app.send_inline_bot_result(
+            chat_id=message.chat.id,
+            query_id=bot_results.query_id,
+            result_id=bot_results.results[0].id,
+        )
+    except Exception:
+        bot_results = await app.get_inline_bot_results(
+            f"@{bot.me.username}", "help_menu_text"
+        )
+        await app.send_inline_bot_result(
+            chat_id=message.chat.id,
+            query_id=bot_results.query_id,
+            result_id=bot_results.results[0].id,
+        )
+    except Exception as e:
+        print(e)
+        return
+
+    try:
+        await message.delete()
+    except:
+        pass
+      
+
+
+@bot.on_callback_query(filters.regex(r"help_(.*?)"))
+@cb_wrapper
+async def help_button(client, query):
+    plug_match = re.match(r"help_plugin(.+?)", query.data)
+    prev_match = re.match(r"help_prev(.+?)", query.data)
+    next_match = re.match(r"help_next(.+?)", query.data)
+    back_match = re.match(r"help_back", query.data)
+
+    # Count the total number of plugins
+    total_plugins = len(plugs)
+
+    top_text = f"""
+**💫 Bad-Userbot Help Menu 👻  » {__version__} ✨
+            ❤️ᴛᴏᴛᴀʟ ᴘʟᴜɢɪɴꜱ: {total_plugins} ❤️***""",
+
+    if plug_match:
+        plugin = plug_match.group(1)
+        text = (
+            "****💫 ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ʜᴇʟᴘ ᴍᴇɴᴜ ᴏꜰ \n💕 ᴘʟᴜɢɪɴ ✨ ** {}\n".format(
+                plugs[plugin].__NAME__
+            )
+            + plugs[plugin].__MENU__
+        )
+        key = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="↪️ Back", callback_data="help_back"
+                    ),
+                    InlineKeyboardButton(
+                        text="🌐 Group Support", url="https://t.me/your_group_support"
+                    ),
+                    InlineKeyboardButton(
+                        text="📢 Channel Support", url="https://t.me/your_channel_support"
+                    ),
+                ],
+            ]
+        )
+
+        await bot.edit_inline_text(
+            query.inline_message_id,
+            text=text,
+            reply_markup=key,
+            disable_web_page_preview=True
+        )
+    elif prev_match:
+        curr_page = int(prev_match.group(1))
+        await bot.edit_inline_text(
+            query.inline_message_id,
+            text=top_text,
+            reply_markup=InlineKeyboardMarkup(
+                paginate_plugins(curr_page - 1, plugs, "help")
+            ),
+            disable_web_page_preview=True,
+        )
+
+    elif next_match:
+        next_page = int(next_match.group(1))
+        await bot.edit_inline_text(
+            query.inline_message_id,
+            text=top_text,
+            reply_markup=InlineKeyboardMarkup(
+                paginate_plugins(next_page + 1, plugs, "help")
+            ),
+            disable_web_page_preview=True,
+        )
+
+    elif back_match:
+        await bot.edit_inline_text(
+            query.inline_message_id,
+            text=top_text,
+            reply_markup=InlineKeyboardMarkup(
+                paginate_plugins(0, plugs, "help")
+            ),
+            disable_web_page_preview=True,
+        )
