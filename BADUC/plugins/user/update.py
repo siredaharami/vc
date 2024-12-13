681d24270095794c77b73ff72e6538d251956ab1
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-13 07:54:15 +0530

    Create update.py

---
+++ BADUC/plugins/user/update.py
@@ -0,0 +1,64 @@
+import aiohttp, aiofiles, asyncio, base64, logging
+import os, platform, random, re, socket
+import sys, time, textwrap
+from os import getenv
+from io import BytesIO
+from time import strftime
+from functools import partial
+from dotenv import load_dotenv
+from datetime import datetime
+
+from BADUC import SUDOERS
+from BADUC.core.clients import app
+from BADUC.core.command import *
+
+from typing import Union, List, Pattern
+from git import Repo
+from git.exc import GitCommandError, InvalidGitRepositoryError
+
+
+
+
+@app.on_message(bad(["gitpull"]) & (filters.me | filters.user(SUDOERS)))
+async def update_repo_latest(client, message):
+    response = await message.reply_text("Checking for available updates...")
+    try:
+        repo = Repo()
+    except GitCommandError:
+        return await response.edit("Git Command Error")
+    except InvalidGitRepositoryError:
+        return await response.edit("Invalid Git Repsitory")
+    to_exc = f"git fetch origin main &> /dev/null"
+    os.system(to_exc)
+    await asyncio.sleep(7)
+    verification = ""
+    REPO_ = repo.remotes.origin.url.split(".git")[0]  # main git repository
+    for checks in repo.iter_commits(f"HEAD..origin/main"):
+        verification = str(checks.count())
+    if verification == "":
+        return await response.edit("Bot is up-to-date!")
+    updates = ""
+    ordinal = lambda format: "%d%s" % (
+        format,
+        "tsnrhtdd"[(format // 10 % 10 != 1) * (format % 10 < 4) * format % 10 :: 4],
+    )
+    for info in repo.iter_commits(f"HEAD..origin/main"):
+        updates += f"<b>➣ #{info.count()}: [{info.summary}]({REPO_}/commit/{info}) by -> {info.author}</b>\n\t\t\t\t<b>➥ Commited on:</b> {ordinal(int(datetime.fromtimestamp(info.committed_date).strftime('%d')))} {datetime.fromtimestamp(info.committed_date).strftime('%b')}, {datetime.fromtimestamp(info.committed_date).strftime('%Y')}\n\n"
+    _update_response_ = "<b>A new update is available for the userbot!</b>\n\n➣ Pushing Updates Now</code>\n\n**<u>Updates:</u>**\n\n"
+    _final_updates_ = _update_response_ + updates
+    if len(_final_updates_) > 4096:
+        link = await paste_queue(updates)
+        url = link + "/index.txt"
+        nrs = await response.edit(
+            f"<b>A new update is available for the userbot!</b>\n\n➣ Pushing Updates Now</code>\n\n**<u>Updates:</u>**\n\n[Click Here to checkout Updates]({url})"
+        )
+    else:
+        nrs = await response.edit(_final_updates_, disable_web_page_preview=True)
+    os.system("git stash &> /dev/null && git pull")
+    await response.edit(
+        f"{nrs.text}\n\nuserbot was updated successfully! Now, wait for 1 - 2 mins until the userbot reboots!"
+    )
+    os.system("pip3 install -r requirements.txt --force-reinstall")
+    os.system(f"kill -9 {os.getpid()} && python3 -m BADUC")
+    sys.exit()
+    return
