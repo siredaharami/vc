0d389c0eafdd6359cae2083b88879242e107fa38
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-14 21:49:59 +0530

    Create translate.py

---
+++ BADUC/plugins/user/translate.py
@@ -0,0 +1,45 @@
+from pyrogram import filters
+from pyrogram.types import *
+from gpytranslate import Translator
+from BADUC import SUDOERS
+from BADUC.core.clients import app
+from BADUC.core.command import *
+
+trans = Translator()
+
+@app.on_message(bad(["tr"]) & (filters.me | filters.user(SUDOERS)))
+
+async def translate(_, message) -> None:
+    reply_msg = message.reply_to_message
+    if not reply_msg:
+        await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ᴛʀᴀɴsʟᴀᴛᴇ ɪᴛ !")
+        return
+    if reply_msg.caption:
+        to_translate = reply_msg.caption
+    elif reply_msg.text:
+        to_translate = reply_msg.text
+    try:
+        args = message.text.split()[1].lower()
+        if "//" in args:
+            source = args.split("//")[0]
+            dest = args.split("//")[1]
+        else:
+            source = await trans.detect(to_translate)
+            dest = args
+    except IndexError:
+        source = await trans.detect(to_translate)
+        dest = "en"
+    translation = await trans(to_translate, sourcelang=source, targetlang=dest)
+    reply = (
+        f"ᴛʀᴀɴsʟᴀᴛᴇᴅ ғʀᴏᴍ {source} to {dest}:\n"
+        f"{translation.text}"
+    )
+    await message.reply_text(reply)
+    
+    
+    
+    __NAME__ = "Tʀᴀɴsʟᴀᴛᴇ"
+__MENU__ = """
+`.tr` - **Translate some text by give a text or reply that text/caption.**
+
+"""
