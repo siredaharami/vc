873a02357f05e696e192ea6a34dc8f241e9270fa
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-15 08:24:26 +0530

    Update translate.py

--- BADUC/plugins/user/translate.py
+++ BADUC/plugins/user/translate.py
@@ -8,16 +8,21 @@ from BADUC.core.command import *
 trans = Translator()
 
 @app.on_message(bad(["tr"]) & (filters.me | filters.user(SUDOERS)))
-
 async def translate(_, message) -> None:
     reply_msg = message.reply_to_message
     if not reply_msg:
-        await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ᴛʀᴀɴsʟᴀᴛᴇ ɪᴛ !")
+        await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ᴛʀᴀɴsʟᴀᴛᴇ ɪᴛ!")
+        await message.delete()
         return
     if reply_msg.caption:
         to_translate = reply_msg.caption
     elif reply_msg.text:
         to_translate = reply_msg.text
+    else:
+        await message.reply_text("ɴᴏ ᴛᴇxᴛ ғᴏᴜɴᴅ ᴛᴏ ᴛʀᴀɴsʟᴀᴛᴇ!")
+        await message.delete()
+        return
+
     try:
         args = message.text.split()[1].lower()
         if "//" in args:
@@ -29,17 +34,20 @@ async def translate(_, message) -> None:
     except IndexError:
         source = await trans.detect(to_translate)
         dest = "en"
-    translation = await trans(to_translate, sourcelang=source, targetlang=dest)
-    reply = (
-        f"ᴛʀᴀɴsʟᴀᴛᴇᴅ ғʀᴏᴍ {source} to {dest}:\n"
-        f"{translation.text}"
-    )
-    await message.reply_text(reply)
-    
-    
-    
-    __NAME__ = "Tʀᴀɴsʟᴀᴛᴇ"
-__MENU__ = """
-`.tr` - **Translate some text by give a text or reply that text/caption.**
 
+    try:
+        translation = await trans(to_translate, sourcelang=source, targetlang=dest)
+        reply = (
+            f"ᴛʀᴀɴsʟᴀᴛᴇᴅ ғʀᴏᴍ {source} ᴛᴏ {dest}:\n"
+            f"{translation.text}"
+        )
+        await reply_msg.reply_text(reply)
+    except Exception as e:
+        await message.reply_text(f"Error: {e}")
+    finally:
+        await message.delete()
+
+__NAME__ = "Tʀᴀɴsʟᴀᴛᴇ"
+__MENU__ = """
+`.tr` - **Translate some text by giving a language code or replying to a text/caption.**
 """
