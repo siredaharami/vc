869c97d8db9d1db31bcea67b245aab148f78ccc3
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-15 09:55:01 +0530

    Update update.py

--- BADUC/plugins/user/update.py
+++ BADUC/plugins/user/update.py
@@ -33,6 +33,7 @@ async def update_repo_latest(client, message):
         return await response.edit("Git Command Error")
     except InvalidGitRepositoryError:
         return await response.edit("Invalid Git Repository")
+    
     to_exc = f"git fetch origin main &> /dev/null"
     os.system(to_exc)
     await asyncio.sleep(7)
@@ -41,7 +42,10 @@ async def update_repo_latest(client, message):
     for checks in repo.iter_commits(f"HEAD..origin/main"):
         verification = str(checks.count())
     if verification == "":
-        return await response.edit("userbot is up-to-date!")
+        await response.edit("userbot is up-to-date!")
+        await message.delete()  # Delete command message
+        return
+
     updates = ""
     ordinal = lambda format: "%d%s" % (
         format,
@@ -49,6 +53,7 @@ async def update_repo_latest(client, message):
     )
     for info in repo.iter_commits(f"HEAD..origin/main"):
         updates += f"<b>➣ #{info.count()}: [{info.summary}]({REPO_}/commit/{info}) by -> {info.author}</b>\n\t\t\t\t<b>➥ Committed on:</b> {ordinal(int(datetime.fromtimestamp(info.committed_date).strftime('%d')))} {datetime.fromtimestamp(info.committed_date).strftime('%b')}, {datetime.fromtimestamp(info.committed_date).strftime('%Y')}\n\n"
+    
     _update_response_ = "<b>A new update is available for the userbot!</b>\n\n➣ Pushing Updates Now</code>\n\n**<u>Updates:</u>**\n\n"
     _final_updates_ = _update_response_ + updates
     if len(_final_updates_) > 4096:
@@ -59,20 +64,22 @@ async def update_repo_latest(client, message):
         )
     else:
         nrs = await response.edit(_final_updates_, disable_web_page_preview=True)
+    
     os.system("git stash &> /dev/null && git pull")
     await response.edit(
         f"{nrs.text}\n\nuserbot was updated successfully! Now, wait for 1 - 2 mins until the userbot reboots!"
     )
+    
     os.system("pip3 install -r requirements.txt --force-reinstall")
     os.system(f"kill -9 {os.getpid()} && python3 -m BADUC")
     sys.exit()
-    return
-
+    await message.delete()  # Delete command message
 
 @app.on_message(bad(["sh"]) & (filters.me | filters.user(SUDOERS)))
 async def shellrunner(_, message: Message):
     if len(message.command) < 2:
         return await edit_or_reply(message, text="<b>ᴇxᴀᴍᴩʟᴇ :</b>\n/sh git pull")
+    
     text = message.text.split(None, 1)[1]
     if "\n" in text:
         code = text.split("\n")
@@ -107,8 +114,10 @@ async def shellrunner(_, message: Message):
                 message, text=f"<b>ERROR :</b>\n<pre>{''.join(errors)}</pre>"
             )
         output = process.stdout.read()[:-1].decode("utf-8")
+    
     if str(output) == "\n":
         output = None
+    
     if output:
         if len(output) > 4096:
             with open("output.txt", "w+") as file:
@@ -120,17 +129,18 @@ async def shellrunner(_, message: Message):
                 caption="<code>Output</code>",
             )
             return os.remove("output.txt")
+        
         await edit_or_reply(message, text=f"<b>OUTPUT :</b>\n<pre>{output}</pre>")
     else:
         await edit_or_reply(message, text="<b>OUTPUT :</b>\n<code>None</code>")
-        
-
+    
+    await message.delete()  # Delete command message
 
 __NAME__ = " Aᴅᴍɪɴ "
 __MENU__ = """
-`.ban` ** ban  user **
-`.unban` **unban l user**
+`.ban` **ban user**
+`.unban` **unban user**
 `.mute` **mute user**
 `.unmute` **unmute user**
-`.tmute` **timing mute user**
-"""      
+`.tmute` **timed mute user**
+"""
