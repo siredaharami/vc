f071b4fe07ff938995a90fd68c37afb1ee9e39ac
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-15 14:06:09 +0530

    Create clone.py

---
+++ BADUC/plugins/user/clone.py
@@ -0,0 +1,56 @@
+import os
+
+from pyrogram import *
+from pyrogram.types import *
+
+from BADUC import SUDOERS
+from BADUC.core.clients import app
+from BADUC.core.command import *
+
+from BADUC.database.basic import edit_or_reply, get_text, get_user
+
+
+
+OWNER = os.environ.get("OWNER", None)
+BIO = os.environ.get("BIO", "404 : Bio Lost")
+
+
+@app.on_message(bad(["clone"]) & (filters.me | filters.user(SUDOERS)))
+async def clone(client: Client, message: Message):
+    text = get_text(message)
+    op = await message.edit_text("`Cloning`")
+    userk = get_user(message, text)[0]
+    user_ = await client.get_users(userk)
+    if not user_:
+        await op.edit("`Whom i should clone:(`")
+        return
+
+    get_bio = await client.get_chat(user_.id)
+    f_name = user_.first_name
+    c_bio = get_bio.bio
+    pic = user_.photo.big_file_id
+    poto = await client.download_media(pic)
+
+    await client.set_profile_photo(photo=poto)
+    await client.update_profile(
+        first_name=f_name,
+        bio=c_bio,
+    )
+    await message.edit(f"**From now I'm** __{f_name}__")
+
+
+@app.on_message(bad(["unclone"]) & (filters.me | filters.user(SUDOERS)))
+async def revert(client: Client, message: Message):
+    await message.edit("`unclone`")
+    r_bio = BIO
+
+    # Get ur Name back
+    await client.update_profile(
+        first_name=OWNER,
+        bio=r_bio,
+    )
+    # Delte first photo to get ur identify
+    photos = [p async for p in client.get_chat_photos("me")]
+    await client.delete_profile_photos(photos[0].file_id)
+    await message.edit("`I am back!`")
+
