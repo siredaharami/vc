da48d4fd4b452897f7c5767fbf4954ed077cf654
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-16 19:05:26 +0530

    Update raids.py

--- BADUC/plugins/user/raids.py
+++ BADUC/plugins/user/raids.py
@@ -319,42 +319,43 @@ async def raid(Client: Client, m: Message):
 
 @app.on_message(bad(["raid"]) & (filters.me | filters.user(SUDOERS)))
 async def raid(Client: Client, m: Message):  
-      Bad = "".join(m.text.split(maxsplit=1)[1:]).split(" ", 2)
-      if len(Bad) == 2:
+    Bad = "".join(m.text.split(maxsplit=1)[1:]).split(" ", 2)
+    if len(Bad) == 2:
         counts = int(Bad[0])
         username = Bad[1]
         if not counts:
-          await m.reply_text(f"RAID LIMIT NOT FOUND PLEASE GIVE COUNT!")
-          return       
+            await m.reply_text(f"RAID LIMIT NOT FOUND PLEASE GIVE COUNT!")
+            return       
         if not username:
-          await m.reply_text("you need to specify an user! Reply to any user or gime id/username")
-          return
+            await m.reply_text("you need to specify an user! Reply to any user or give id/username")
+            return
         try:
-           user = await Client.get_users(Bad[1])
+            user = await Client.get_users(Bad[1])
         except:
-           await m.reply_text("**Error:** User not found or may be deleted!")
-           return
-      elif m.reply_to_message:
+            await m.reply_text("**Error:** User not found or may be deleted!")
+            return
+    elif m.reply_to_message:
         counts = int(Bad[0])
         try:
-           user = await Client.get_users(m.reply_to_message.from_user.id)
+            user = await Client.get_users(m.reply_to_message.from_user.id)
         except:
-           user = m.reply_to_message.from_user 
-      else:
+            user = m.reply_to_message.from_user 
+    else:
         await m.reply_text("Usage: .raid count username or reply")
         return
-      if int(m.chat.id) in GROUP:
-         await m.reply_text("**Sorry !! i Can't Spam Here.**")
-         return
-      if int(user.id) in VERIFIED_USERS:
-         await m.reply_text("I can't raid on my developer")
-         return
-      if int(user.id) in SUDO_USER:
-         await m.reply_text("This guy is a sudo users.")
-         return
-      mention = user.mention
-      for _ in range(counts): 
-         r = f"{mention} {choice(RAID)}"
-         await Client.send_message(m.chat.id, r)
-         await asyncio.sleep(0.3).send_message(m.chat.id, r)
-         await asyncio.sleep(0.3)
+
+    if int(m.chat.id) in GROUP:
+        await m.reply_text("**Sorry !! I can't Spam Here.**")
+        return
+    if int(user.id) in VERIFIED_USERS:
+        await m.reply_text("I can't raid on my developer")
+        return
+    if int(user.id) in SUDO_USER:
+        await m.reply_text("This guy is a sudo user.")
+        return
+    
+    mention = user.mention
+    for _ in range(counts): 
+        r = f"{mention} {choice(RAID)}"
+        await Client.send_message(m.chat.id, r)
+        await asyncio.sleep(0.3)
