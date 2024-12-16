0d85e237f88c45e365e340f1bec9d55fd9ad3fe4
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-16 18:33:33 +0530

    Update raids.py

--- BADUC/plugins/user/raids.py
+++ BADUC/plugins/user/raids.py
@@ -184,3 +184,176 @@ async def emoji(x: Client, e: Message):
 
 
 # DM RAID
+
+
+@app.on_message(bad(["praid"]) & (filters.me | filters.user(SUDOERS)))
+async def raid(Client: Client, m: Message):  
+      Bad = "".join(m.text.split(maxsplit=1)[1:]).split(" ", 2)
+      if len(Bad) == 2:
+        counts = int(Bad[0])
+        username = Bad[1]
+        if not counts:
+          await m.reply_text(f"PBIRAID LIMIT NOT FOUND PLEASE GIVE COUNT!")
+          return       
+        if not username:
+          await m.reply_text("you need to specify an user! Reply to any user or gime id/username")
+          return
+        try:
+           user = await Client.get_users(Bad[1])
+        except:
+           await m.reply_text("**Error:** User not found or may be deleted!")
+           return
+      elif m.reply_to_message:
+        counts = int(Bad[0])
+        try:
+           user = await Client.get_users(m.reply_to_message.from_user.id)
+        except:
+           user = m.reply_to_message.from_user 
+      else:
+        await m.reply_text("Usage: .pbiraid count username or reply")
+        return
+      if int(m.chat.id) in GROUP:
+         await m.reply_text("**Sorry !! i Can't Spam Here.**")
+         return
+      if int(user.id) in VERIFIED_USERS:
+         await m.reply_text("I can't Pbiraid on my developer")
+         return
+      if int(user.id) in SUDO_USER:
+         await m.reply_text("This guy is a sudo users.")
+         return
+      mention = user.mention
+      for _ in range(counts): 
+         r = f"{mention} {choice(PBIRAID)}"
+         await Client.send_message(m.chat.id, r)
+         await asyncio.sleep(0.3)
+
+
+#oneword
+
+
+@app.on_message(bad(["oraid"]) & (filters.me | filters.user(SUDOERS)))
+async def raid(Client: Client, m: Message):  
+      Bad = "".join(m.text.split(maxsplit=1)[1:]).split(" ", 2)
+      if len(Bad) == 2:
+        counts = int(Bad[0])
+        username = Bad[1]
+        if not counts:
+          await m.reply_text(f"ONEWORDRAID LIMIT NOT FOUND PLEASE GIVE COUNT!")
+          return       
+        if not username:
+          await m.reply_text("you need to specify an user! Reply to any user or gime id/username")
+          return
+        try:
+           user = await Client.get_users(Bad[1])
+        except:
+           await m.reply_text("**Error:** User not found or may be deleted!")
+           return
+      elif m.reply_to_message:
+        counts = int(Bad[0])
+        try:
+           user = await Client.get_users(m.reply_to_message.from_user.id)
+        except:
+           user = m.reply_to_message.from_user 
+      else:
+        await m.reply_text("Usage: .oneraid count username or reply")
+        return
+      if int(m.chat.id) in GROUP:
+         await m.reply_text("**Sorry !! i Can't Spam Here.**")
+         return
+      if int(user.id) in VERIFIED_USERS:
+         await m.reply_text("I can't oneraid on my developer")
+         return
+      if int(user.id) in SUDO_USER:
+         await m.reply_text("This guy is a sudo users.")
+         return
+      mention = user.mention
+      for _ in range(counts): 
+         r = f"{mention} {choice(OneWord)}"
+         await Client.send_message(m.chat.id, r)
+         await asyncio.sleep(0.3)
+
+#HIRAID
+
+@app.on_message(bad(["hraid"]) & (filters.me | filters.user(SUDOERS)))
+async def raid(Client: Client, m: Message):  
+      Bad = "".join(m.text.split(maxsplit=1)[1:]).split(" ", 2)
+      if len(Bad) == 2:
+        counts = int(Bad[0])
+        username = Bad[1]
+        if not counts:
+          await m.reply_text(f"HIRAID LIMIT NOT FOUND PLEASE GIVE COUNT!")
+          return       
+        if not username:
+          await m.reply_text("you need to specify an user! Reply to any user or gime id/username")
+          return
+        try:
+           user = await Client.get_users(Bad[1])
+        except:
+           await m.reply_text("**Error:** User not found or may be deleted!")
+           return
+      elif m.reply_to_message:
+        counts = int(Bad[0])
+        try:
+           user = await Client.get_users(m.reply_to_message.from_user.id)
+        except:
+           user = m.reply_to_message.from_user 
+      else:
+        await m.reply_text("Usage: .hiraid count username or reply")
+        return
+      if int(m.chat.id) in GROUP:
+         await m.reply_text("**Sorry !! i Can't Spam Here.**")
+         return
+      if int(user.id) in VERIFIED_USERS:
+         await m.reply_text("I can't hiraid on my developer")
+         return
+      if int(user.id) in SUDO_USER:
+         await m.reply_text("This guy is a sudo users.")
+         return
+      mention = user.mention
+      for _ in range(counts): 
+         r = f"{mention} {choice(HIRAID)}"
+         await Client.send_message(m.chat.id, r)
+         await asyncio.sleep(0.3)
+
+
+@app.on_message(bad(["raid"]) & (filters.me | filters.user(SUDOERS)))
+async def raid(Client: Client, m: Message):  
+      Bad = "".join(m.text.split(maxsplit=1)[1:]).split(" ", 2)
+      if len(Bad) == 2:
+        counts = int(Bad[0])
+        username = Bad[1]
+        if not counts:
+          await m.reply_text(f"RAID LIMIT NOT FOUND PLEASE GIVE COUNT!")
+          return       
+        if not username:
+          await m.reply_text("you need to specify an user! Reply to any user or gime id/username")
+          return
+        try:
+           user = await Client.get_users(Bad[1])
+        except:
+           await m.reply_text("**Error:** User not found or may be deleted!")
+           return
+      elif m.reply_to_message:
+        counts = int(Bad[0])
+        try:
+           user = await Client.get_users(m.reply_to_message.from_user.id)
+        except:
+           user = m.reply_to_message.from_user 
+      else:
+        await m.reply_text("Usage: .raid count username or reply")
+        return
+      if int(m.chat.id) in GROUP:
+         await m.reply_text("**Sorry !! i Can't Spam Here.**")
+         return
+      if int(user.id) in VERIFIED_USERS:
+         await m.reply_text("I can't raid on my developer")
+         return
+      if int(user.id) in SUDO_USER:
+         await m.reply_text("This guy is a sudo users.")
+         return
+      mention = user.mention
+      for _ in range(counts): 
+         r = f"{mention} {choice(RAID)}"
+         await Client.send_message(m.chat.id, r)
+         await asyncio.sleep(0.3).send_message(m.chat.id, r)
+         await asyncio.sleep(0.3)
