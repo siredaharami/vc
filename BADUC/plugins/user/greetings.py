1fd8192efb9b09e8a04d169be74e3a69b563d55f
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-17 21:31:41 +0530

    Update greetings.py

--- BADUC/plugins/user/greetings.py
+++ BADUC/plugins/user/greetings.py
@@ -1,15 +1,16 @@
 from pyrogram import Client, filters
 from pyrogram.types import ChatMemberUpdated
-
-from BADUC import SUDOERS
 from BADUC.core.clients import app
-from BADUC.core.command import *
+
 
 @app.on_chat_member_updated(filters.group)
-def welcome_new_user(client, member_update: ChatMemberUpdated):
-    if member_update.new_chat_member and member_update.new_chat_member.status == "member":
-        user_id = member_update.new_chat_member.user.id
-        first_name = member_update.new_chat_member.user.first_name or "User"
-        
-        welcome_message = f"🌟 **Welcome to the group, [{first_name}](tg://user?id={user_id})!** 🌟\n\nFeel free to chat and engage here!"
-        client.send_message(chat_id=member_update.chat.id, text=welcome_message)
+def welcome_new_user(client, update: ChatMemberUpdated):
+    # Check if the new user just joined the group
+    if update.new_chat_member.status == "member" and update.old_chat_member.status not in ["member", "administrator", "creator"]:
+        user = update.new_chat_member.user
+        user_name = user.first_name or "User"
+        user_id = user.id
+
+        # Send a welcome message with user ID and name
+        welcome_message = f"🌟 **Welcome to the group, [{user_name}](tg://user?id={user_id})!** 🌟"
+        client.send_message(update.chat.id, welcome_message)
