47938bec64a24dcc8eebb7194aafc4b80111a284
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-18 21:58:03 +0530

    Update ping.py

--- BADUC/plugins/clone2/ping.py
+++ BADUC/plugins/clone2/ping.py
@@ -14,32 +14,22 @@ def load_clone_data():
     return {}
 
 # Your restricted_command function (for limiting access)
-@Client.on_message(filters.command([]))  # Capture all commands
-async def restricted_command(Client: Client, msg: Message):
-    command = msg.text.split()[0].lstrip("/")
-    clone_data = load_clone_data()
-
-    # Check if the user is the owner of any cloned Client
-    for token, details in clone_data.items():
-        if details["owner_id"] == msg.from_user.id:
-            # The user is the owner, allow them to use the command
-            return  # Command logic can be placed here for execution
-        else:
-            await msg.reply("You are not the owner of this cloned bot, so you cannot use its commands!")
-            return
-
-# The new 'ping' command for the cloned bot
 @Client.on_message(filters.command("ping"))
 async def ping(Client: Client, msg: Message):
     user_id = msg.from_user.id
     clone_data = load_clone_data()
 
-    # Check if the user is the owner of any cloned bot
+    # Check if the user is the owner of any cloned Client
     for token, details in clone_data.items():
-        if details["owner_id"] == user_id:
-            # If the user is the owner, send a ping response
-            await msg.reply("🏓 Pong! You are the owner of this bot.")
-            return
+        # Check if owner_id exists in the clone data
+        if "owner_id" in details:
+            if details["owner_id"] == user_id:
+                # If the user is the owner, send a ping response
+                await msg.reply("🏓 Pong! You are the owner of this bot.")
+                return
+        else:
+            # If owner_id is missing in the clone data, log a warning
+            print(f"Warning: 'owner_id' missing for bot token {token}")
 
     # If the user is not the owner, deny access to the ping command
     await msg.reply("You are not the owner of this cloned bot, so you cannot use the ping command!")
