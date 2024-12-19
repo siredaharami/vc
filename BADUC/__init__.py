6490a4e77aef378d6a5fe62d3955ed04ebf8d125
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-19 19:03:24 +0530

    Update __init__.py

--- BADUC/__init__.py
+++ BADUC/__init__.py
@@ -16,6 +16,7 @@ __version__ = "v3.0.0"
 spam_chats = []
 SUDOERS = SUDOERS
 SUDOERS.append(OWNER_ID)
+CLONE_OWNERS = {}
 
 mongo_async_cli = _mongo_async_(MONGO_DB_URL)
 mongodb = mongo_async_cli.badmundaxdb
@@ -55,3 +56,41 @@ plugs = PLUGINS
 # Variables
 from BADUC.core import config
 vars = config
+
+
+#clone
+
+async def load_clone_owners():
+    async for entry in cloneownerdb.find():
+        bot_id = entry["bot_id"]
+        user_id = entry["user_id"]
+        CLONE_OWNERS[bot_id] = user_id
+
+async def save_clonebot_owner(bot_id, user_id):
+    await cloneownerdb.update_one(
+        {"bot_id": bot_id},
+        {"$set": {"user_id": user_id}},
+        upsert=True
+    )
+async def get_clone_owner(bot_id):
+    data = await cloneownerdb.find_one({"bot_id": bot_id})
+    if data:
+        return data["user_id"]
+    return None
+
+async def delete_clone_owner(bot_id):
+    await cloneownerdb.delete_one({"bot_id": bot_id})
+    CLONE_OWNERS.pop(bot_id, None)
+
+async def save_idclonebot_owner(clone_id, user_id):
+    await cloneownerdb.update_one(
+        {"clone_id": clone_id},
+        {"$set": {"user_id": user_id}},
+        upsert=True
+    )
+
+async def get_idclone_owner(clone_id):
+    data = await cloneownerdb.find_one({"clone_id": clone_id})
+    if data:
+        return data["user_id"]
+    return None
