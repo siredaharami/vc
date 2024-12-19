143156b1305cd1ca4ee24caf3559cffcef2fd197
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-19 19:50:04 +0530

    Update ping.py

--- BADUC/plugins/clone2/ping.py
+++ BADUC/plugins/clone2/ping.py
@@ -3,7 +3,8 @@ from BADUC.plugins.bot.clone3 import get_bot_owner  # Import the function to che
 
 @Client.on_message(filters.command("ping"))
 async def ping_command(client, message):
-    bot_id = message.chat.id  # Get the current bot's ID
+    bot_info = await client.get_me()  # Retrieve current bot's details
+    bot_id = bot_info.id  # Get the current bot's ID
     user_id = message.from_user.id  # Get the user's ID
 
     # Check if the user is authorized to use this bot
