d0098725558e2dd0cf1b5488dd0f0815b0f6f8b7
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-20 23:05:26 +0530

    Update config.py

--- BADUC/core/config.py
+++ BADUC/core/config.py
@@ -25,7 +25,7 @@ PM_GUARD_TEXT = getenv("PM_GUARD_TEXT", "**👋🏻𝐇ყ !**\n❤️𝐎ɯɳҽ
 PM_GUARD_LIMIT = int(getenv("PM_GUARD_LIMIT", 3))
 PM_PIC = getenv("PM_PIC", "https://files.catbox.moe/rix20t.jpg")
 BOT_PICTURE_URL = "https://files.catbox.moe/83d5lc.jpg"
-MUST_JOIN_GROUPS = ["https://t.me/HEROKUBIN_01", "https://t.me/PBX_CHAT", "https://t.me/ll_BAD_MUNDA_WORLD_ll", "https://t.me/ll_BAD_ABOUT_ll", "https://t.me/BEAUTIFUl_DPZ"]
+MUST_JOIN_GROUPS = ["-1002122353644", "-1001693569838", "-1002056907061", "-1002020205239", "-1002138886770"]
 
 # Don't Edit This Codes From This Line
 
