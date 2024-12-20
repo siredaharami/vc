529d1603ab31d1a0efa4da9e75bdbf482b2b258d
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-20 10:05:29 +0530

    Delete BADUC/plugins/clone2/test.py

--- BADUC/plugins/clone2/test.py
+++
@@ -1,5 +0,0 @@
-from pyrogram import Client, filters
-
-@Client.on_message(filters.command("test"))
-async def test_command(client, message):
-    await message.reply("The plugin is working correctly!")
