e9beda4c293436aae30a1b78fb53b28816c63dce
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-21 22:27:34 +0530

    Update and rename ping to adminlist.py

---
+++ BADUC/plugins/clone/adminlist.py
@@ -0,0 +1,157 @@
+import html
+
+from pyrogram import Client, enums, filters
+from pyrogram.types import Message
+
+from BADUC.database.basic import edit_or_reply
+from BADUC.database.parser import mention_html, mention_markdown
+
+from BADUC import SUDOERS
+from BADUC.core.command import *
+
+
+@Client.on_message(bad(["admins", "adminlist"]) & (filters.me | filters.user(SUDOERS)))
+async def adminlist(Client: Client, message: Message):
+    replyid = None
+    toolong = False
+    if len(message.text.split()) >= 2:
+        chat = message.text.split(None, 1)[1]
+        grup = await Client.get_chat(chat)
+    else:
+        chat = message.chat.id
+        grup = await Client.get_chat(chat)
+    if message.reply_to_message:
+        replyid = message.reply_to_message.id
+    creator = []
+    admin = []
+    badmin = []
+    async for a in Client.get_chat_members(
+        message.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS
+    ):
+        try:
+            nama = a.user.first_name + " " + a.user.last_name
+        except:
+            nama = a.user.first_name
+        if nama is None:
+            nama = "☠️ Deleted account"
+        if a.status == enums.ChatMemberStatus.ADMINISTRATOR:
+            if a.user.is_bot:
+                badmin.append(mention_markdown(a.user.id, nama))
+            else:
+                admin.append(mention_markdown(a.user.id, nama))
+        elif a.status == enums.ChatMemberStatus.OWNER:
+            creator.append(mention_markdown(a.user.id, nama))
+    admin.sort()
+    badmin.sort()
+    totaladmins = len(creator) + len(admin) + len(badmin)
+    teks = "**Admins in {}**\n".format(grup.title)
+    teks += "╒═══「 Creator 」\n"
+    for x in creator:
+        teks += "│ • {}\n".format(x)
+        if len(teks) >= 4096:
+            await message.reply(message.chat.id, teks, reply_to_message_id=replyid)
+            teks = ""
+            toolong = True
+    teks += "╞══「 {} Human Administrator 」\n".format(len(admin))
+    for x in admin:
+        teks += "│ • {}\n".format(x)
+        if len(teks) >= 4096:
+            await message.reply(message.chat.id, teks, reply_to_message_id=replyid)
+            teks = ""
+            toolong = True
+    teks += "╞══「 {} Bot Administrator 」\n".format(len(badmin))
+    for x in badmin:
+        teks += "│ • {}\n".format(x)
+        if len(teks) >= 4096:
+            await message.reply(message.chat.id, teks, reply_to_message_id=replyid)
+            teks = ""
+            toolong = True
+    teks += "╘══「 Total {} Admins 」".format(totaladmins)
+    if toolong:
+        await message.reply(message.chat.id, teks, reply_to_message_id=replyid)
+    else:
+        await message.edit(teks)
+
+
+@Client.on_message(bad(["reportadmin", "reportadmins", "report"]) & (filters.me | filters.user(SUDOERS)))
+async def report_admin(Client: Client, message: Message):
+    await message.delete()
+    if len(message.text.split()) >= 2:
+        text = message.text.split(None, 1)[1]
+    else:
+        text = None
+    grup = await Client.get_chat(message.chat.id)
+    admin = []
+    async for a in Client.get_chat_members(
+        message.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS
+    ):
+        if (
+            a.status == enums.ChatMemberStatus.ADMINISTRATOR
+            or a.status == enums.ChatMemberStatus.OWNER
+        ):
+            if not a.user.is_bot:
+                admin.append(mention_html(a.user.id, "\u200b"))
+    if message.reply_to_message:
+        if text:
+            teks = "{}".format(text)
+        else:
+            teks = "{} reported to admins.".format(
+                mention_html(
+                    message.reply_to_message.from_user.id,
+                    message.reply_to_message.from_user.first_name,
+                )
+            )
+    else:
+        if text:
+            teks = "{}".format(html.escape(text))
+        else:
+            teks = "Calling admins in {}.".format(grup.title)
+    teks += "".join(admin)
+    if message.reply_to_message:
+        await Client.send_message(
+            message.chat.id,
+            teks,
+            reply_to_message_id=message.reply_to_message.id,
+            parse_mode=enums.ParseMode.HTML,
+        )
+    else:
+        await Client.send_message(
+            message.chat.id, teks, parse_mode=enums.ParseMode.HTML
+        )
+
+
+
+
+@Client.on_message(bad(["bots", "botlist"]) & (filters.me | filters.user(SUDOERS)))
+async def get_list_bots(Client: Client, message: Message):
+    replyid = None
+    if len(message.text.split()) >= 2:
+        chat = message.text.split(None, 1)[1]
+        grup = await Client.get_chat(chat)
+    else:
+        chat = message.chat.id
+        grup = await Client.get_chat(chat)
+    if message.reply_to_message:
+        replyid = message.reply_to_message.id
+    getbots = Client.get_chat_members(chat)
+    bots = []
+    async for a in getbots:
+        try:
+            nama = a.user.first_name + " " + a.user.last_name
+        except:
+            nama = a.user.first_name
+        if nama is None:
+            nama = "☠️ Deleted account"
+        if a.user.is_bot:
+            bots.append(mention_markdown(a.user.id, nama))
+    teks = "**All bots in group {}**\n".format(grup.title)
+    teks += "╒═══「 Bots 」\n"
+    for x in bots:
+        teks += "│ • {}\n".format(x)
+    teks += "╘══「 Total {} Bots 」".format(len(bots))
+    if replyid:
+        await Client.send_message(message.chat.id, teks, reply_to_message_id=replyid)
+    else:
+        await message.edit(teks)
+
+        
