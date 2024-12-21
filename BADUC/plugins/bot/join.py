09c290822315aec75a81d1aa3a7b743b5aaa596c
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-21 09:12:38 +0530

    Update join.py

--- BADUC/plugins/bot/join.py
+++ BADUC/plugins/bot/join.py
@@ -11,33 +11,40 @@ async def must_join_channel(bot: Client, msg: Message):
     if not MUST_JOIN:
         return
 
+    missing_links = []
+    buttons = []
+
     for channel in MUST_JOIN:
         try:
+            # Check if the user is a member of the channel/group
+            await bot.get_chat_member(channel, msg.from_user.id)
+        except UserNotParticipant:
             try:
-                # Check if the user is a member of the channel/group
-                await bot.get_chat_member(channel, msg.from_user.id)
-            except UserNotParticipant:
+                # Generate the invite link
                 if channel.isalpha():
                     link = f"https://t.me/{channel}"
                 else:
                     chat_info = await bot.get_chat(channel)
                     link = chat_info.invite_link
 
-                # Ensure the link is valid before sending
+                # Add link and button to the list
                 if link:
-                    try:
-                        await msg.reply_photo(
-                            photo="https://envs.sh/Tn_.jpg",
-                            caption=(
-                                f"๏ ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ "
-                                f"[ᴊᴏɪɴ]({link}) ʏᴇᴛ. ᴘʟᴇᴀsᴇ ᴊᴏɪɴ ᴛᴏ ᴜsᴇ ᴍʏ ꜰᴇᴀᴛᴜʀᴇs."
-                            ),
-                            reply_markup=InlineKeyboardMarkup(
-                                [[InlineKeyboardButton("Join", url=link)]]
-                            )
-                        )
-                        await msg.stop_propagation()
-                    except ChatWriteForbidden:
-                        pass
-        except ChatAdminRequired:
-            print(f"๏ Please promote me as an admin in the chat: {channel}!")
+                    missing_links.append(link)
+                    buttons.append([InlineKeyboardButton(f"Join {channel}", url=link)])
+            except ChatAdminRequired:
+                print(f"๏ Please promote me as an admin in the chat: {channel}!")
+
+    # Send a single message with all missing links and buttons
+    if missing_links:
+        try:
+            await msg.reply_photo(
+                photo="https://envs.sh/Tn_.jpg",
+                caption=(
+                    "๏ ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ ᴛʜᴇꜱᴇ ᴄʜᴀɴɴᴇʟꜱ/ɢʀᴏᴜᴘꜱ ʏᴇᴛ:\n\n"
+                    + "\n".join([f"[Join {channel}]({link})" for channel, link in zip(MUST_JOIN, missing_links)])
+                ),
+                reply_markup=InlineKeyboardMarkup(buttons)
+            )
+            await msg.stop_propagation()
+        except ChatWriteForbidden:
+            pass
