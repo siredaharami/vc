80077a2784cd3d23073b8696b8895a1f5413d5a5
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-21 10:38:50 +0530

    Update join.py

--- BADUC/plugins/bot/join.py
+++ BADUC/plugins/bot/join.py
@@ -3,14 +3,15 @@ from BADUC.core.clients import bot
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 
-# List of mandatory channels/groups (can dynamically add more groups/channels here)
-MUST_JOIN = ["PBX_CHAT", "HEROKUBIN_01", "ll_BAD_ABOUT_ll", "BEAUTIFUl_DPZ", "ll_BAD_MUNDA_ll", "ll_BAD_MUNDA_WORLD_ll"]
+# List of mandatory channels/groups
+MUST_JOIN = ["PBX_CHAT", "HEROKUBIN_01", "ll_BAD_ABOUT_ll", "BEAUTIFUl_DPZ", "ll_BAD_MUNDA_ll"]
 
 @bot.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
     if not MUST_JOIN:
         return
 
+    missing_channels = []  # To store missing channel names
     buttons = []  # To store button objects
 
     for channel in MUST_JOIN:
@@ -28,17 +29,20 @@ async def must_join_channel(bot: Client, msg: Message):
 
                 # Add link and button to the list
                 if link:
+                    missing_channels.append(channel)
                     buttons.append([InlineKeyboardButton(f"ᴊᴏɪɴ {channel}", url=link)])
             except ChatAdminRequired:
                 print(f"๏ ᴘʟᴇᴀꜱᴇ ᴘʀᴏᴍᴏᴛᴇ ᴍᴇ ᴀꜱ ᴀɴ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴇ ᴄʜᴀᴛ {channel}!")
 
     # Send a single message with all missing links and buttons
-    if buttons:
+    if missing_channels:
         try:
+            # Make sure all buttons are added to the InlineKeyboardMarkup
             await msg.reply_photo(
                 photo="https://files.catbox.moe/2kporf.jpg",
                 caption=(
-                    "๏ ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ, ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ ᴛʜᴇꜱᴇ ᴄʜᴀɴɴᴇʟꜱ/ɢʀᴏᴜᴘꜱ ʏᴇᴛ. ᴘʟᴇᴀꜱᴇ ᴊᴏɪɴ ᴛᴏ ᴜsᴇ ᴍʏ ꜰᴇᴀᴛᴜʀᴇꜱ."
+                    "๏ ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ, ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ ᴛʜᴇꜱᴇ ᴄʜᴀɴɴᴇʟꜱ/ɢʀᴏᴜᴘꜱ ʏᴇᴛ:\n\n"
+                    + "\n".join([f"[ᴊᴏɪɴ {channel}](https://t.me/{channel})" for channel in missing_channels])
                 ),
                 reply_markup=InlineKeyboardMarkup(buttons)
             )
